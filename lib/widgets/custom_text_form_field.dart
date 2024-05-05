796650cfb2045a4163f5354289ac5bdaf3c5999b
Moatasem mohamed <moatasemmohamed39@gmail>
2024-05-05 23:29:38 +0300

    [FIX] text field direction issue

--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart'as intl;
 import 'package:notes/core/extension/context_extension.dart';
 class CustomTextFormField extends StatelessWidget {
   final int ? maxLines;
@@ -12,7 +13,7 @@ class CustomTextFormField extends StatelessWidget {
   final List<TextInputFormatter>? formatter;
   final String? Function(String? value)? validator;
   final AutovalidateMode ?autoValidateMode;
-  final TextEditingController ?controller;
+  final TextEditingController controller;
   const CustomTextFormField({
     super.key,
     this.maxLines,
@@ -25,29 +26,41 @@ class CustomTextFormField extends StatelessWidget {
     this.formatter,
     this.validator,
     this.autoValidateMode,
-    this.controller
+    required this.controller
   });
+
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller:controller,
-      autofocus:autoFocus,
-      autovalidateMode: autoValidateMode,
-      maxLines: maxLines,
-      minLines: minLines,
-      keyboardType: TextInputType.multiline,
-      inputFormatters: formatter,
-      style: style,
-      onChanged: onChange,
-      validator: validator,
-      cursorColor: Theme.of(context).primaryColorLight,
-      decoration: InputDecoration(
-        isDense: true,
-        isCollapsed: true,
-        constraints: BoxConstraints(maxWidth: context.width - 35),
-        hintText: hintText,
-        hintStyle: hintStyle,
-      ),
+    intl.TextDirection dir=intl.Bidi.detectRtlDirectionality(controller.text)?intl.TextDirection.RTL:intl.TextDirection.LTR;
+    final ValueNotifier<intl.TextDirection> textDir = ValueNotifier(dir);
+    return ValueListenableBuilder<intl.TextDirection>(
+      builder: (context, intl.TextDirection value, child) {
+        return TextFormField(
+          controller:controller,
+          textDirection:value==intl.TextDirection.RTL?TextDirection.rtl:TextDirection.ltr,
+          autofocus:autoFocus,
+          autovalidateMode: autoValidateMode,
+          maxLines: maxLines,
+          minLines: minLines,
+          keyboardType: TextInputType.multiline,
+          inputFormatters: formatter,
+          style: style,
+          onChanged: (input){
+            intl.TextDirection direction=intl.Bidi.detectRtlDirectionality(controller.text)?intl.TextDirection.RTL:intl.TextDirection.LTR;
+            if (direction != value) textDir.value = direction;
+            onChange!(input);
+          },
+          validator: validator,
+          cursorColor: Theme.of(context).primaryColorLight,
+          decoration: InputDecoration(
+            isDense: true,
+            isCollapsed: true,
+            constraints: BoxConstraints(maxWidth: context.width - 35),
+            hintText: hintText,
+            hintStyle: hintStyle,
+          ),
+        );
+      }, valueListenable: textDir,
     );
   }
 }
