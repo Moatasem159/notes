e4a5a2c3814ff815ad00ae2cbc686fdfacab7ecf
Moatasem mohamed <moatasemmohamed39@gmail>
2024-05-21 14:42:35 +0300

    [FIX] handle notification when app is turned off

--- lib/core/notification/app_notification.dart
+++ lib/core/notification/app_notification.dart
@@ -3,38 +3,41 @@ import 'package:flutter/material.dart';
 import 'package:notes/config/routes/app_routes.dart';
 import 'package:notes/core/extension/context_extension.dart';
 import 'package:notes/models/note.dart';
+
 abstract class NotificationsConstants {
   static String channelName = "Schedule channel";
   static String channelKey = "Schedule key";
   static String channelDescription = "Schedule Notification channel";
 }
+
 class NotificationManager {
   static initializeNotifications() async {
-    await AwesomeNotifications()
-        .initialize("resource://drawable/notification", [
-      NotificationChannel(
-        channelKey: NotificationsConstants.channelKey,
-        channelName: NotificationsConstants.channelName,
-        channelDescription: NotificationsConstants.channelDescription,
-        playSound: true,
-        channelShowBadge: true,
-        icon: "resource://drawable/notification",
-        defaultPrivacy: NotificationPrivacy.Public,
-        defaultRingtoneType: DefaultRingtoneType.Notification,
-        importance: NotificationImportance.High,
-      ),
-    ],
-
+    await AwesomeNotifications().initialize(
+      "resource://drawable/notification",
+      [
+        NotificationChannel(
+          channelKey: NotificationsConstants.channelKey,
+          channelName: NotificationsConstants.channelName,
+          channelDescription: NotificationsConstants.channelDescription,
+          playSound: true,
+          channelShowBadge: true,
+          icon: "resource://drawable/notification",
+          defaultPrivacy: NotificationPrivacy.Public,
+          defaultRingtoneType: DefaultRingtoneType.Notification,
+          importance: NotificationImportance.High,
+        ),
+      ],
     );
-    await AwesomeNotifications().getInitialNotificationAction(removeFromActionEvents: false);
   }
+
   static void _showPermissionDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (_) {
         return AlertDialog(
           title: const Text('Notification Permission'),
-          content: const Text('This app needs permission to send notifications.'),
+          content:
+              const Text('This app needs permission to send notifications.'),
           actions: [
             TextButton(
               child: const Text('Don\'t Allow'),
@@ -45,7 +48,9 @@ class NotificationManager {
             TextButton(
               child: const Text('Allow'),
               onPressed: () {
-                AwesomeNotifications().requestPermissionToSendNotifications().then((_) {
+                AwesomeNotifications()
+                    .requestPermissionToSendNotifications()
+                    .then((_) {
                   context.pop();
                 });
               },
@@ -55,60 +60,83 @@ class NotificationManager {
       },
     );
   }
-  static Future<bool> checkNotificationPermission(BuildContext context) async{
-    bool isAllow=false;
-    await AwesomeNotifications().isNotificationAllowed().then((isAllowed)async {
+
+  static Future<bool> checkNotificationPermission(BuildContext context) async {
+    bool isAllow = false;
+    await AwesomeNotifications()
+        .isNotificationAllowed()
+        .then((isAllowed) async {
       isAllow = isAllowed;
     });
-   return isAllow;
+    return isAllow;
   }
-  static createNotification(BuildContext context, {required NotificationContent content,NotificationSchedule? schedule}) async{
-      await checkNotificationPermission(context).then((value) {
-        if(value){
-          AwesomeNotifications().createNotification(
-            content: content,
-            schedule:schedule
-          );
-        }
-        else{
+
+  static createNotification(BuildContext context,
+      {required NotificationContent content,
+      NotificationSchedule? schedule}) async {
+    await checkNotificationPermission(context).then(
+      (value) {
+        if (value) {
+          AwesomeNotifications()
+              .createNotification(content: content, schedule: schedule);
+        } else {
           _showPermissionDialog(context);
         }
-      },);
+      },
+    );
   }
 
-  static handleNotificationAction()async{
+  static handleBackground() async {
+    ReceivedAction? action = await AwesomeNotifications()
+        .getInitialNotificationAction(removeFromActionEvents: false);
+    if (action != null) {
+      dynamic payload = action.payload;
+      Note note = Note.fromMap(payload);
+      AppRouter.navigatorKey.currentState?.pushReplacementNamed(Routes.noteRoute, arguments: note);
+    }
+  }
+
+  static handleNotificationAction() async {
     await AwesomeNotifications().setListeners(
-      onNotificationDisplayedMethod: NotificationController.onNotificationDisplayedMethod,
-      onNotificationCreatedMethod: NotificationController.onNotificationCreatedMethod,
-      onDismissActionReceivedMethod: NotificationController.onDismissActionReceivedMethod,
-      onActionReceivedMethod: NotificationController.onActionReceivedMethod);
+        onNotificationDisplayedMethod:
+            NotificationController.onNotificationDisplayedMethod,
+        onNotificationCreatedMethod:
+            NotificationController.onNotificationCreatedMethod,
+        onDismissActionReceivedMethod:
+            NotificationController.onDismissActionReceivedMethod,
+        onActionReceivedMethod: NotificationController.onActionReceivedMethod);
   }
 }
-class NotificationController {
 
+class NotificationController {
   /// Use this method to detect when a new notification or a schedule is created
   @pragma("vm:entry-point")
-  static Future <void> onNotificationCreatedMethod(ReceivedNotification receivedNotification) async {
+  static Future<void> onNotificationCreatedMethod(
+      ReceivedNotification receivedNotification) async {
     // Your code goes here
   }
 
   /// Use this method to detect every time that a new notification is displayed
   @pragma("vm:entry-point")
-  static Future <void> onNotificationDisplayedMethod(ReceivedNotification receivedNotification) async {
+  static Future<void> onNotificationDisplayedMethod(
+      ReceivedNotification receivedNotification) async {
     // Your code goes here
   }
 
   /// Use this method to detect if the user dismissed a notification
   @pragma("vm:entry-point")
-  static Future <void> onDismissActionReceivedMethod(ReceivedAction receivedAction) async {
-    // Your code goes here
+  static Future<void> onDismissActionReceivedMethod(
+      ReceivedAction receivedAction) async {
+
   }
 
   /// Use this method to detect when the user taps on a notification or action button
   @pragma("vm:entry-point")
-  static Future <void> onActionReceivedMethod(ReceivedAction receivedAction) async {
-    dynamic payload=receivedAction.payload;
-    Note note=Note.fromMap(payload);
-    AppRouter.navigatorKey.currentState?.pushReplacementNamed(Routes.noteRoute,arguments: note);
+  static Future<void> onActionReceivedMethod(
+      ReceivedAction receivedAction) async {
+    dynamic payload = receivedAction.payload;
+    Note note = Note.fromMap(payload);
+    AppRouter.navigatorKey.currentState
+        ?.pushReplacementNamed(Routes.noteRoute, arguments: note);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:notes/app/app.dart';
 import 'package:notes/app/injection_container.dart'as di show init;
 import 'package:notes/config/routes/app_routes.dart';
+import 'package:notes/core/notification/app_notification.dart';
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding=WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await di.init();
   runApp(Nota(appRouter: AppRouter()));
+  await NotificationManager.handleBackground();
+  FlutterNativeSplash.remove();
 }
